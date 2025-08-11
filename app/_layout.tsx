bb82b7cf63716726a9e450c7e44121fcdb15b0e0
iredox <idreesadam200@gmail.com>
2025-08-11 14:41:22 +0100

    fix: native wind error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import '../global.css';
 import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
